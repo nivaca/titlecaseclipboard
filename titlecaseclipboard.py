263201e5793801fb647afbf3d78d0a21164c1d50
Nicolas Vaughan <nivaca@fastmail.com>
2021-09-10 18:23:00 -0500

    first commit

---
+++ titlecaseclipboard.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+""" titlecaseclipboard.py
+Usage:
+    - First way:  titlecaseclipboard.py this is the sentence
+        Writes to standard output: This Is the Sentence
+    - Second way: titlecaseclipboard.py
+        Takes the text in the clipboard (if any), converts it to
+        smart title case, copies it back to the clipboard,
+        and writes it to standard output.
+"""
+
+import sys
+import pyperclip
+from titlecase import titlecase
+
+
+# -------------------------------------------------------------------
+def main():
+    """ Main function. """
+    clipboard = False
+    argv = sys.argv[1:]
+
+    if not argv or argv[0] == '':
+        # try clipboard
+        inputtext = pyperclip.paste()
+        if inputtext != "".strip():
+            clipboard = True
+        else:
+            # no arguments, no clipboard text
+            sys.exit(1)
+    else:
+        inputtext = ' '.join(argv)
+        inputtext = inputtext.strip()
+
+    outputtext = titlecase(inputtext)
+
+    if clipboard:
+        pyperclip.copy(outputtext)
+
+    print(outputtext, file=sys.stdout)
+
+
+# -----------------------------------------------------------------
+if __name__ == '__main__':
+    main()
